3644fbf0195994e10fc98a7a65f0024dec148feb
Lili Chen <lilnic@ksu.edu>
2022-11-04 10:19:27 -0500

    deleted some auto-gen stuff

--- Music_API/Controllers/EXAMPLE_WeatherForecastController.cs
+++ Music_API/Controllers/EXAMPLE_WeatherForecastController.cs
@@ -4,22 +4,22 @@ namespace Music_API.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class EXAMPLE_WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<EXAMPLE_WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public EXAMPLE_WeatherForecastController(ILogger<EXAMPLE_WeatherForecastController> logger)
         {
             _logger = logger;
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<WeatherForecast> Get()   // there was a WeatherForecast.cs class auto generated like this one too but I killed him
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
